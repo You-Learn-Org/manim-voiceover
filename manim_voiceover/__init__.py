da40edf2730760d0beb87efaffcdfbc2b2a79e2e
Sacha <55644767+SoldierSacha@users.noreply.github.com>
2026-02-11 23:22:31 -0500

    work

--- manim_voiceover/__init__.py
+++ manim_voiceover/__init__.py
@@ -1,5 +1,6 @@
 from manim_voiceover.tracker import VoiceoverTracker
 from manim_voiceover.voiceover_scene import VoiceoverScene
+from manim_voiceover.voiceover_extractor import VoiceoverExtractor
 
 from importlib.metadata import version
 
